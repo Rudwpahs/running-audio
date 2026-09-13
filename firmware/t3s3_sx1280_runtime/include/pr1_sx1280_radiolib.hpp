c8325f948cc48a27d9a4a50c986cd243cbf64127
이현준 <110889799+Rudwpahs@users.noreply.github.com>
2026-09-14 01:05:49 +0900

    refactor: remove unsupported FLRC SNR adapter

--- firmware/t3s3_sx1280_runtime/include/pr1_sx1280_radiolib.hpp
+++ firmware/t3s3_sx1280_runtime/include/pr1_sx1280_radiolib.hpp
@@ -21,7 +21,6 @@ class Sx1280RadioLibPort final : public RadioPort {
                              std::size_t* length) override;
   bool transmit(const std::uint8_t* data, std::size_t length) override;
   std::int16_t rssiDbm() override;
-  std::int16_t snrDb() override;
   std::uint32_t nowMicros() const override;
   void setRxIrqHandler(RxIrqHandler handler, void* context) override;
 
